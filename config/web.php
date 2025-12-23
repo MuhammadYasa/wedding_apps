c59a44cf622875b951516eb666a5df916c0e7ba0
Muhammad Yasa <nearhxh@gmail.com>
2025-12-23 11:52:20 +0700

    - Enable pretty URLs with custom routing rules
    - Enable pretty URL without 'index.php'
    - Add slug-based routing for invitation pages
    - Configure pattern: 'invitation/<slug:[\w\-]+>' => 'invitation/view'
    - Add default controller/action routing rules

--- config/web.php
+++ config/web.php
@@ -42,14 +42,19 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                // Invitation friendly URLs
+                'invitation/<slug:[\w\-]+>' => 'invitation/view',
+                
+                // Default rules
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
